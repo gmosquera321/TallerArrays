09c805e2236010f37cf870cf5669b955fb43e9bb
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-11-11 00:48:21 -0500

    desarrollo

--- TALLERARRAYS/TALLERARRAYS/Program.cs
+++ TALLERARRAYS/TALLERARRAYS/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Dado un vector de tamaño N, llenar el vector aleatoriamente con números enteros. Pedir por pantalla un número y determinar si ese número se encuentra en el vector, en caso afirmativo, indicar su posición (índice) dentro del vector, en caso negativo, mostrar el mensaje “El número no existe”.
+using System;
+public class parial2
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el tamaño del vector: ");
+        int N = int.Parse(Console.ReadLine());
+
+
+    }
